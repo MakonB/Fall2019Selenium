6fe69795f954eeb4468eb4a9f06711e3f62751cc
beibitbaimuratov <bbaimurat@gmail.com>
2020-03-01 13:54:15 -0500

    basic functions

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -6,12 +6,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigation {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver  = new ChromeDriver();
 
         driver.get("http://google.com");
+
+        Thread.sleep(2000);
+
+        String title = driver.getTitle();
+        String ectTitle = "Google";
+
+        if (title.equals(ectTitle)){
+            System.out.println("Test passed");
+        }else{
+            System.out.println("Failed");
+        }
+
+        driver.close();
 
 
 
