0ba8f0ed290ef1469be725a40ae59b203f65a69d
beibitbaimuratov <bbaimurat@gmail.com>
2020-03-09 07:26:27 -0400

    IFrame test

---
+++ src/test/java/com/automation/tests/day5/IFrame.java
@@ -0,0 +1,35 @@
+package com.automation.tests.day5;
+
+import com.automation.utilities.BrowserUtils;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class IFrame {
+
+    public static void main(String[] args) {
+
+
+        WebDriverManager.chromedriver().version("79").setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://practice.cybertekschool.com/iframe");
+        BrowserUtils.wait(2);
+        driver.switchTo().frame("mce_0_ifr");
+
+        WebElement id = driver.findElement(By.id("tinymce"));
+        System.out.println(id.getText());
+        id.clear();
+        id.sendKeys("Java !!!");
+
+        driver.switchTo().defaultContent();
+
+        System.out.println(driver.findElement(By.tagName("h3")).getText());
+
+
+
+        driver.quit();
+    }
+}
